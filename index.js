590063e221962bc743bc0bbfb5a41968687306cd
kemoik <ck.caleb17@gmail.com>
2022-06-02 14:28:57 +0300

    Added multiobjects and two functions for order and production

---
+++ index.js
@@ -0,0 +1,9 @@
+let stocks ={
+    Fruits : ["Strawberry" , "Grapes" , "Banana" , "Apple", "Bluberry"],
+    Liquid : ["Water" , "Ice"],
+    Holder : ["Cone" , "Cup" ,"Stick"],
+    Toppings: ["Choclate" , "Peanuts" , "Sprinkles"],
+
+};
+let order = () => {};
+let production = () => {};
